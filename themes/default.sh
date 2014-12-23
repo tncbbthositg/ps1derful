eae3a41390ff5cba456fa0cb36e525345bfbd0a8
Patrick Caldwell <tncbbthositg@gmail.com>
2014-12-23 12:08:50 -0500

    allowing override with GIT_AUTHOR_NAME environment variable

--- themes/default.sh
+++ themes/default.sh
@@ -8,11 +8,15 @@ source $__theme_source_dirname/../ps1derful_helpers.sh
 ps1derful_set_bash_prompt(){
     local b="$light_gray"
     local path=`__ps1derful_get_short_path`
-	
-	local user_name="${user_name:-`git config user.name`}"
+
+    local user_name=$GIT_AUTHOR_NAME
+  	if [ -z $user_name ]; then
+        local user_name="${user_name:-`git config user.name`}"
+    fi
+
     local last_commit_time=`ps1derful_get_git_last_commit "$user_name"`
     local changed_file_count=`ps1derful_get_git_change_count`
-	
+
     local git_prompt=`__git_ps1 " { %s | Δ $changed_file_count | $last_commit_time | $user_name }"`
 
     local clock=`binary_clock_get_time`
